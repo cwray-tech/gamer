6a9fcbfc7d9ed8d92efe9f6de295cc69b5d528d4
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-07-04 21:30:20 -0600

    added api calls for homepage

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,18 +11,51 @@ class GameController extends Controller
     public function index()
     {
         $before = Carbon::now()->subMonths(2)->timestamp;
-        $after = Carbon::now()->addMonths(2)->timestamp;
+        $now = Carbon::now()->timestamp;
+        $fourMonthsFromNow = Carbon::now()->addMonths(4)->timestamp;
+        $oneMonthFromNow = Carbon::now()->addMonth()->timestamp;
 
         $popularGames = Http::withHeaders(config('services.idgb'))->withOptions([
-            'body' => 'fields name, first_release_date, popularity, platforms.abbreviation, rating, cover.url;
-            where platforms = (48,49,138,6);
-            sort popularity desc;'
+            'body' => "fields name, first_release_date, popularity, platforms.abbreviation, rating, cover.url;
+            where platforms = (48,49,138,6)
+            & (first_release_date >= {$before}
+            & first_release_date < {$now});
+            sort popularity desc;"
+        ])->get('https://api-v3.igdb.com/games/')->json();
+
+        $recentlyReleased = Http::withHeaders(config('services.idgb'))->withOptions([
+            'body' => "fields name, first_release_date, popularity, summary, platforms.abbreviation, rating, cover.url;
+             where platforms = (48,49,138,6) & first_release_date < {$now};
+             sort first_release_date desc;
+             limit 2;"
+        ])->get('https://api-v3.igdb.com/games/')->json();
+
+        $mostAnticipated = Http::withHeaders(config('services.idgb'))->withOptions([
+            'body' => "fields name, first_release_date, popularity, summary, platforms.abbreviation, rating, cover.url;
+             where platforms = (48,49,138,6)
+             & first_release_date > {$now}
+             & first_release_date < {$fourMonthsFromNow};
+             sort popularity desc;
+             limit 2;"
+        ])->get('https://api-v3.igdb.com/games/')->json();
+
+        $comingSoon = Http::withHeaders(config('services.idgb'))->withOptions([
+            'body' => "fields name, first_release_date, popularity, summary, platforms.abbreviation, rating, cover.url;
+             where platforms = (48,49,138,6)
+             & first_release_date > {$now}
+             & first_release_date < {$oneMonthFromNow};
+             sort first_release_date desc;
+             limit 2;"
         ])->get('https://api-v3.igdb.com/games/')->json();
 
         return view('.index', [
-            'popularGames' => $popularGames
+            'popularGames' => $popularGames,
+            'recentlyReleased' => $recentlyReleased,
+            'mostAnticipated' => $mostAnticipated,
+            'comingSoon' => $comingSoon
         ]);
     }
+
     public function show()
     {
         return view('.show');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,17 +15,20 @@
                                  alt="game cover"
                                  class="hover:opacity-75 w-full transition ease-in-out duration-150">
                         </a>
-                        <div style="right:-20px; bottom: -20px"
-                             class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
-                        </div>
+                        @if(isset($game['rating']))
+                            <div style="right:-20px; bottom: -20px"
+                                 class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
+                                <div
+                                    class="font-semibold text-xs flex justify-center items-center h-full">{{ round($game['rating']). '%' }}</div>
+                            </div>
+                        @endif
                     </div>
                     <a href=""
                        class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name'] }}</a>
                     <p class="text-gray-400 mt-1">
                         @foreach($game['platforms'] as $platform)
                             @if(array_key_exists('abbreviation', $platform))
-                            {{ $platform['abbreviation'] }},
+                                {{ $platform['abbreviation'] }},
                             @endif
                         @endforeach
                     </p>
@@ -34,82 +37,73 @@
         </div>
         <div class="lg:flex my-10">
             <div class="recently-reviewed lg:w-3/4 lg:mr-32">
-                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Released</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg md:flex p-6 shadow-md">
-                        <div class="relative flex-none">
-                            <a href="">
-                                <img src="/images/ff7.jpg" alt="game cover"
-                                     class="md:w-48 w-full hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div style="right:-20px; bottom: -20px"
-                                 class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full">
-                                <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    @foreach($recentlyReleased as $game)
+                        <div class="game bg-gray-800 rounded-lg md:flex p-6 shadow-md">
+                            <div class="relative flex-none">
+                                <a href="">
+                                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}"
+                                         alt="game cover"
+                                         class="md:w-48 w-full hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+                                @if(isset($game['rating']))
+                                    <div style="right:-20px; bottom: -20px"
+                                         class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full">
+                                        <div
+                                            class="font-semibold text-xs flex justify-center items-center h-full">{{ round($game['rating']). '%' }}</div>
+                                    </div>
+                                @endif
+                            </div>
+                            <div class="md:ml-12">
+                                <a href=""
+                                   class="block text-lg font-semiboindex.blade.phpld leading-tight hover:text-gray-400 mt-4">{{ $game['name']}}</a>
+                                <p class="text-gray-400 mt-1">
+                                    @foreach($game['platforms'] as $platform)
+                                        @if(array_key_exists('abbreviation', $platform))
+                                            {{ $platform['abbreviation'] }},
+                                        @endif
+                                    @endforeach
+                                </p>
+                                <p class="mt-6 text-gray-400">
+                                    {{ $game['summary']  }}
+                                </p>
                             </div>
                         </div>
-                        <div class="md:ml-12">
-                            <a href=""
-                               class="block text-lg font-semiboindex.blade.phpld leading-tight hover:text-gray-400 mt-4">Game
-                                Name</a>
-                            <p class="text-gray-400 mt-1">Playstation 4</p>
-                            <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                Architecto commodi eligendi explicabo nisi, nostrum odit officia qui quia reprehenderit
-                                totam ut voluptate! Accusamus amet animi deserunt dolorem ea eaque earum eos esse et
-                                eveniet excepturi, exercitationem illo ipsam iusto minus numquam odio officia quaerat
-                                quia quod repudiandae unde vero, vitae!
-                            </p>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
             <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-8 mt-8">
+                    @foreach($mostAnticipated as $game)
                     <div class="game flex">
                         <a href="">
-                            <img src="/images/cyberpunk.jpg" alt="game cover"
+                            <img src="{{ $game['cover']['url'] }}" alt="game cover"
                                  class="w-16 hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                         <div class="ml-4">
-                            <a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <a href="" class="hover:text-gray-300">{{ $game['name'] }}</a>
                             <div class="text-gray-400 text-sm mt-1">2020</div>
                         </div>
                     </div>
-                    <div class="game flex">
-                        <a href="">
-                            <img src="/images/cyberpunk.jpg" alt="game cover"
-                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="ml-4">
-                            <a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
-                            <div class="text-gray-400 text-sm mt-1">2020</div>
-                        </div>
-                    </div>
+                    @endforeach
 
                 </div>
                 <h2 class="text-blue-500 uppercase mt-12 tracking-wide font-semibold">Coming Soon</h2>
                 <div class="most-anticipated-container space-y-8 mt-8">
+                    @foreach($comingSoon as $game)
                     <div class="game flex">
-                        <a href="">
-                            <img src="/images/cyberpunk.jpg" alt="game cover"
-                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        <a href="" class="w-1/4">
+                            <img src="{{ $game['cover']['url'] }}" alt="game cover"
+                                 class="w-full hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                         <div class="ml-4">
-                            <a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <a href="" class="hover:text-gray-300">{{ $game['name'] }}</a>
                             <div class="text-gray-400 text-sm mt-1">2020</div>
                         </div>
                     </div>
-                    <div class="game flex">
-                        <a href="">
-                            <img src="/images/cyberpunk.jpg" alt="game cover"
-                                 class="w-16 hover:opacity-75 transition ease-in-out duration-150">
-                        </a>
-                        <div class="ml-4">
-                            <a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
-                            <div class="text-gray-400 text-sm mt-1">2020</div>
-                        </div>
-                    </div>
-
+@endforeach
                 </div>
             </div>
         </div>
